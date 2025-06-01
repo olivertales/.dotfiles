dd25b65168cde81e7bc60c4ec3c6cd302ed64b98
Thalles Oliveira <thalles.gbpereira@gmail.com>
2025-06-01 20:13:13 -0300

    Update Roslyn config

--- init.lua
+++ init.lua
@@ -68,8 +68,10 @@ require('lazy').setup({
     {
         "seblyng/roslyn.nvim",
         ft = "cs",
-        ---@module 'roslyn.config'
-        ---@type RoslynNvimConfig
+        opts = {
+            filewatching = "roslyn",
+            lock_target = true
+        }
     },
 
     --Db connection in neovim
@@ -872,32 +874,39 @@ vim.api.nvim_create_autocmd('LspAttach', {
     end,
 })
 
---Roslyn setup
-require('roslyn').setup {
-    config = {
-        settings = {
-            ["csharp|inlay_hints"] = {
-                csharp_enable_inlay_hints_for_implicit_object_creation = true,
-                csharp_enable_inlay_hints_for_implicit_variable_types = true,
-                csharp_enable_inlay_hints_for_lambda_parameter_types = true,
-                csharp_enable_inlay_hints_for_types = true,
-                dotnet_enable_inlay_hints_for_indexer_parameters = true,
-                dotnet_enable_inlay_hints_for_literal_parameters = true,
-                dotnet_enable_inlay_hints_for_object_creation_parameters = true,
-                dotnet_enable_inlay_hints_for_other_parameters = true,
-                dotnet_enable_inlay_hints_for_parameters = true,
-                dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix = true,
-                dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
-                dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
-            },
-            ["csharp|code_lens"] = {
-                dotnet_enable_references_code_lens = true,
-            },
+--Dotnet LSP and build setup
+vim.lsp.config('roslyn', {
+    on_attach = on_attach,
+    settings = {
+        ["csharp|inlay_hints"] = {
+            csharp_enable_inlay_hints_for_implicit_object_creation = true,
+            csharp_enable_inlay_hints_for_implicit_variable_types = true,
+            csharp_enable_inlay_hints_for_lambda_parameter_types = true,
+            csharp_enable_inlay_hints_for_types = true,
+            dotnet_enable_inlay_hints_for_indexer_parameters = true,
+            dotnet_enable_inlay_hints_for_literal_parameters = true,
+            dotnet_enable_inlay_hints_for_object_creation_parameters = true,
+            dotnet_enable_inlay_hints_for_other_parameters = true,
+            dotnet_enable_inlay_hints_for_parameters = true,
+            dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix = true,
+            dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
+            dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
+        },
+        ["csharp|code_lens"] = {
+            dotnet_enable_references_code_lens = true,
+        },
+        ["csharp|completion"] = {
+            dotnet_show_completion_items_from_unimported_namespaces = true,
+            dotnet_provide_regex_completions = true,
+            dotnet_show_name_completion_suggestions = true
+        },
+        ["csharp|formatting"] = {
+            dotnet_organize_imports_on_format = true
         }
-    },
-    filewatching = "roslyn",
-    lock_target = true
-}
+    }
+})
+vim.g.dotnet_errors_only = true
+vim.g.dotnet_show_project_file = false
 
 --Setup autotag
 require('nvim-ts-autotag').setup({
